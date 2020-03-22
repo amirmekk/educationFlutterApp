29adb0efe0cf412f2c47edbc7c80b3f3a9afc6f4
amir mekkaoui <amirmekk@amirs-MacBook-Pro.local>
2020-03-22 22:11:03 +0100

    forgot to implement the bookmarking function in a section of the app and now i fixed that

--- lib/pages/wad3iyat.dart
+++ lib/pages/wad3iyat.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tarikh19/pages/bookmarks.dart';
 
-class Wad3iyaPage extends StatelessWidget {
+class Wad3iyaPage extends StatefulWidget {
   final List appBarTitleData;
   Wad3iyaPage({this.appBarTitleData});
 
+  @override
+  _Wad3iyaPageState createState() => _Wad3iyaPageState();
+}
+
+class _Wad3iyaPageState extends State<Wad3iyaPage> {
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
           appBar: AppBar(
-            title: Text(appBarTitleData[0]),
+            title: Text(widget.appBarTitleData[0]),
           ),
           body: ListView.builder(
-              itemCount: appBarTitleData[1].length,
+              itemCount: widget.appBarTitleData[1].length,
               itemBuilder: (context, index) {
                 return Card(
                   child: ListTile(
@@ -23,25 +29,11 @@ class Wad3iyaPage extends StatelessWidget {
                           builder: (_) => Directionality(
                                 textDirection: TextDirection.rtl,
                                 child: AlertDialog(
-                                  title: Text('${appBarTitleData[1][index].itemOne}'),
-                                  content: Text('${appBarTitleData[1][index].itemTwo}'),
+                                  title: Text(
+                                      '${widget.appBarTitleData[1][index].itemOne}'),
+                                  content: Text(
+                                      '${widget.appBarTitleData[1][index].itemTwo}'),
                                   actions: <Widget>[
-                                    Container(
-                                      margin: EdgeInsets.all(10),
-                                      child: Row(
-                                        children: <Widget>[
-                                          GestureDetector(
-                                            child: Icon(
-                                              Icons.star_border,
-                                              size: 30,
-                                            ),
-                                            onTap: () {
-                                              print('stared');
-                                            },
-                                          ),
-                                        ],
-                                      ),
-                                    ),
                                     GestureDetector(
                                       child: Icon(
                                         Icons.check_circle,
@@ -56,10 +48,18 @@ class Wad3iyaPage extends StatelessWidget {
                               ));
                     },
                     trailing: IconButton(
-                      icon: Icon(Icons.star_border),
-                      onPressed: () {},
+                      icon: checkIfExist(
+                          widget.appBarTitleData[1][index].itemOne),
+                      onPressed: () {
+                        setState(() {
+                          updateBookmarks(
+                              widget.appBarTitleData[1][index].itemOne,
+                              widget.appBarTitleData[1][index].itemTwo);
+                        });
+                      },
                     ),
-                    subtitle: Text('${appBarTitleData[1][index].itemTwo}'),
+                    subtitle:
+                        Text('${widget.appBarTitleData[1][index].itemTwo}'),
                     isThreeLine: true,
                     leading: Text(
                       '${index + 1}',
@@ -68,7 +68,7 @@ class Wad3iyaPage extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    title: Text('${appBarTitleData[1][index].itemOne}'),
+                    title: Text('${widget.appBarTitleData[1][index].itemOne}'),
                   ),
                 );
               })),
