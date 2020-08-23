f4bd25de6608dcb7bebc968953d10a12564294cf
amir mekkaoui <amirmekk@outlook.com>
2020-08-23 18:53:31 +0100

    fixed the issue where the bookmarked items are gone when restarting the app

--- lib/pages/bookmarks.dart
+++ lib/pages/bookmarks.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 List bookmarkedItems = [];
 
-updateBookmarks(itemOne, itemTwo) {
+updateBookmarks(itemOne, itemTwo) async {
+  final prefs = await SharedPreferences.getInstance();
+  bookmarkedItems = prefs.getString('favoriteList') == null
+      ? []
+      : jsonDecode(prefs.getString('favoriteList'));
   List toBeAdded = [];
   bool isThere = false;
 
@@ -16,22 +22,33 @@ updateBookmarks(itemOne, itemTwo) {
     for (int i = 0; i < bookmarkedItems.length; i++) {
       if (bookmarkedItems[i][0] == itemOne) {
         bookmarkedItems.removeAt(i);
+        prefs.setString('favoriteList', jsonEncode(bookmarkedItems));
       }
     }
     print(bookmarkedItems);
     return Icon(Icons.star_border);
-  }
-  if (!isThere) {
+  } else if (!isThere) {
     toBeAdded.addAll([itemOne, itemTwo]);
     bookmarkedItems.add(toBeAdded);
+    prefs.setString('favoriteList', jsonEncode(bookmarkedItems));
     print(bookmarkedItems);
     return Icon(Icons.star);
   }
 }
 
 checkIfExist(itemOne) {
-  bool isThere = false;
+  //sync bookmarked list with the persisted list !
 
+  getData() async {
+    final prefs = await SharedPreferences.getInstance();
+    bookmarkedItems = prefs.getString('favoriteList') == null
+        ? []
+        : jsonDecode(prefs.getString('favoriteList'));
+  }
+
+  getData();
+
+  bool isThere = false;
   for (int i = 0; i < bookmarkedItems.length; i++) {
     if (bookmarkedItems[i][0] == itemOne) {
       isThere = true;
@@ -53,6 +70,23 @@ class Bookmarks extends StatefulWidget {
 }
 
 class _BookmarksState extends State<Bookmarks> {
+  List bookmarkedItems = [];
+  @override
+  void initState() {
+    super.initState();
+    getStoredData();
+  }
+
+  getStoredData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List data = prefs.getString('favoriteList') == null
+        ? []
+        : jsonDecode(prefs.getString('favoriteList'));
+    setState(() {
+      bookmarkedItems = data;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
@@ -61,7 +95,9 @@ class _BookmarksState extends State<Bookmarks> {
         appBar: AppBar(title: Text('المفضلة')),
         body: Container(
             child: bookmarkedItems.isEmpty
-                ? Center(child: Text('قم باضافة عناصر الى المفضلة لتتمكن من رؤيتهم هنا'))
+                ? Center(
+                    child: Text(
+                        'قم باضافة عناصر الى المفضلة لتتمكن من رؤيتهم هنا'))
                 : ListView.builder(
                     itemBuilder: (context, index) => Card(
                       child: ListTile(
@@ -70,6 +106,8 @@ class _BookmarksState extends State<Bookmarks> {
                         trailing: IconButton(
                             icon: Icon(Icons.delete_forever),
                             onPressed: () {
+                              updateBookmarks(bookmarkedItems[index][0],
+                                  bookmarkedItems[index][1]);
                               setState(() {
                                 bookmarkedItems.removeAt(index);
                                 print(bookmarkedItems);
